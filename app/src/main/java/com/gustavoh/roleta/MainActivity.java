07df442c4c1acb6aca86a5f2fa8c6c087e115acb
WDGustavo <WDGustavo@github.com>
2021-03-04 22:49:30 -0300

    TarefadeComparaçãoroleta

--- app/src/main/java/com/gustavoh/roleta/MainActivity.java
+++ app/src/main/java/com/gustavoh/roleta/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -14,11 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView parabains = findViewById(R.id.imageView);
+        parabains.setVisibility(View.INVISIBLE);
     }
+
     public void Roletar(View view){
         TextView numero = findViewById(R.id.textView2);
+        TextView teste3 = findViewById(R.id.textView3);
+        ImageView parabains = findViewById(R.id.imageView);
         Random num = new Random();
         int valor = num.nextInt(10)+1;
-        numero.setText("Número: " +valor);
+        EditText teste = findViewById(R.id.editTextTextPersonName);
+        int teste4;
+        teste4 = Integer.parseInt(teste.getText().toString());
+
+        if (teste4 == valor){
+            numero.setText("");
+            parabains.setVisibility(View.VISIBLE);
+        }
+        else {
+            numero.setText("Os são diferentes");
+        }
+        teste3.setText("Número: "+valor);
     }
 }
